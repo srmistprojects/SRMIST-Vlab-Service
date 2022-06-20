fc831248d2bd35debf6ccafac79dac7183218d87
kunalkeshan <kunalkeshan12@gmail.com>
2022-06-21 00:30:42 +0530

    updates fixed

--- controllers/experiment.controller.js
+++ controllers/experiment.controller.js
@@ -23,12 +23,38 @@ ExperimentController.fetchExperimentsWithSubjectId = async (req, res, next) => {
         if (!subjectId) throw new ApiError({ message: 'Cannot fetch experiments without subject Id', statusCode: 400 });
         if (!isValidObjectId(subjectId)) throw new ApiError({ message: 'Given subject Id is invalid', statusCode: 422 });
 
-        const experiments = await Experiment.find({ subject: subjectId }).populate('subject', 'name').lean();
+        const experiments = await Experiment
+            .find({ subject: subjectId })
+            .populate('subject', 'name')
+            .select('name _id')
+            .lean();
         return res.status(200).json({
             message: 'Experiments fetched successfully',
             data: { experiments },
             success: true
         });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+ExperimentController.fetchExperimentWithExperimentId = async (req, res, next) => {
+    // Collecting Required information
+    const { experimentId } = req.params;
+    try {
+        // Sanity Checking
+        if (!experimentId) throw new ApiError({ message: 'Cannot fetch experiments without experiment Id', statusCode: 400 });
+        if (!isValidObjectId(experimentId)) throw new ApiError({ message: 'Given experiment Id is invalid', statusCode: 422 });
+
+        const experiment = await Experiment
+            .findById(experimentId)
+            .populate('subject', 'name')
+            .lean();
+        return res.status(200).json({
+            message: 'Experiment fetched successfully',
+            data: { experiment },
+            success: true,
+        })
     } catch (error) {
         return next(error);
     }
--- routes/experiment.routes.js
+++ routes/experiment.routes.js
@@ -7,6 +7,7 @@ const Router = require('express').Router();
 const experimentController = require('../controllers/experiment.controller');
 
 Router.get('/all/:subjectId', experimentController.fetchExperimentsWithSubjectId);
+Router.get('/:experimentId', experimentController.fetchExperimentWithExperimentId);
 
 // Exporting Router
 module.exports = Router;
